b139295ae44b0f433a16ffffdbebbc53cb5ed499
Mauro Meneghin <m3m0m2@gmail.com>
2020-11-04 23:37:14 +0000

    added two classes around prussdrv: Pru, PrussDrv

---
+++ core/pru.cpp
@@ -0,0 +1,141 @@
+#include "pru.hpp"
+#include "pruss_intc_mapping.h"
+
+#include <iostream>
+#include <assert.h>
+
+
+static void assert_valid_pru_num(int pru_num)
+{
+    assert(pru_num>=0 and pru_num<TOTAL_PRUS_NUM);
+}
+
+
+///
+// Pru
+//
+
+Pru::Pru(int pru_num): _pru_num(pru_num), _pru_mem(nullptr),
+                _ext_mem(nullptr), _ext_mem_size(0)
+{
+    assert_valid_pru_num(_pru_num);
+
+    map_pru_mem();
+
+}
+
+Pru::~Pru()
+{
+    prussdrv_pru_disable(_pru_num);
+}
+
+void Pru::map_pru_mem()
+{
+    if(_pru_mem!=nullptr)
+        return;
+    void* pru_mem_void = nullptr;
+
+    prussdrv_map_prumem (_pru_num, &pru_mem_void);
+
+    _pru_mem = (uint32_t*)pru_mem_void;
+}
+
+void Pru::map_ext_mem()
+{
+    if(_ext_mem!=nullptr)
+        return;
+    void* ext_mem_void;
+
+    prussdrv_map_extmem(&ext_mem_void);
+    _ext_mem = (uint32_t*)ext_mem_void;
+    _ext_mem_size = prussdrv_extmem_size();
+}
+
+void Pru::exec_program_file(const char* file)
+{
+    assert(prussdrv_exec_program(_pru_num, file));
+
+}
+
+void Pru::reset()
+{
+    prussdrv_pru_reset(_pru_num);
+}
+
+void Pru::enable()
+{
+    prussdrv_pru_enable(_pru_num);
+}
+
+void Pru::disable()
+{
+    prussdrv_pru_disable(_pru_num);
+}
+
+
+
+
+///
+// PrussDrv
+//
+
+PrussDrv::PrussDrv()
+{
+    for(int num=0; num<TOTAL_PRUS_NUM; num++)
+        _prus[num] = nullptr;
+
+    prussdrv_init();
+
+    prussdrv_pruintc_init(&_prussintc_init_data);
+}
+
+PrussDrv::~PrussDrv()
+{
+    for(int num=0; num<TOTAL_PRUS_NUM; num++)
+        if(_prus[num] != nullptr)
+            delete _prus[num];
+
+    prussdrv_exit();
+}
+
+void PrussDrv::setup_intc(tpruss_intc_initdata prussintc_init_data)
+{
+    assert(!_init_done);
+    _prussintc_init_data = prussintc_init_data;
+}
+
+Pru& PrussDrv::get_pru(int num)
+{
+    assert_valid_pru_num(num);
+
+    if(_prus[num] == nullptr)
+        _prus[num] = new Pru(num);
+
+    return *_prus[num];
+}
+
+void PrussDrv::open_sysevt(unsigned sysevt)
+{
+    int host = prussdrv_get_event_to_host_map(sysevt);
+    assert(host>=0);
+    int ret = prussdrv_open(host);
+    assert(ret>=0);
+}
+
+void PrussDrv::pru_send_event(unsigned sysevt)
+{
+    prussdrv_pru_send_event(sysevt);
+}
+
+void wait_event_and_clear(unsigned sysevt)
+{
+    int host = prussdrv_get_event_to_host_map(sysevt);
+    assert(host>=0);
+    prussdrv_pru_wait_event(host);
+    prussdrv_pru_clear_event(host, sysevt);
+}
+
+
+Pru* PrussDrv::_prus[TOTAL_PRUS_NUM] = { nullptr, nullptr };
+tpruss_intc_initdata PrussDrv::_prussintc_init_data = PRUSS_INTC_INITDATA;
+bool PrussDrv::_init_done = false;
---
+++ core/pru.hpp
@@ -0,0 +1,106 @@
+/*
+ * @brief classes around prussdrv
+ * @author Mauro Meneghin (m3m0m2@gmail.com)
+ */
+
+#ifndef INCLUDE_PRU_HPP
+#define INCLUDE_PRU_HPP
+
+#define TOTAL_PRUS_NUM  2
+
+#include "prussdrv.h"
+#include <cstdint>
+
+class PrussDrv;
+
+///
+// Pru is non-copyable and retrieved using PrussDrv
+//
+class Pru
+{
+    int _pru_num;
+    uint32_t *_pru_mem, *_ext_mem;
+    uint32_t _ext_mem_size;
+
+    Pru(int pru_num);
+    friend PrussDrv;
+
+    void map_pru_mem();
+
+public:
+    int get_pru_num() const {return _pru_num;}
+    uint32_t* get_pru_mem() {return _pru_mem;}
+    uint32_t* get_ext_mem() {return _ext_mem;}
+    uint32_t get_ext_mem_size() {return _ext_mem_size;}
+
+    ~Pru();
+
+    void map_ext_mem();
+    void exec_program_file(const char* file);
+
+    void reset();
+    void enable();
+    void disable();
+
+    // other functions are available in prussdrv like
+    // prussdrv_pru_write_memory
+
+
+    // prevent copies of this
+    Pru(Pru const&)            = delete;
+    void operator=(Pru const&) = delete;
+};
+
+
+///
+// PrussDrv is a singleton class 
+//
+class PrussDrv
+{
+private:
+    PrussDrv();
+
+public:
+    // call before the first get_instance if needed, otherwise default setup
+    static void setup_intc(tpruss_intc_initdata prussintc_init_data);
+
+    static PrussDrv& get_instance()
+    {
+        static PrussDrv instance;
+        _init_done = true;
+        return instance;
+    }
+    ~PrussDrv();
+
+    // prevent copies of this
+    PrussDrv(PrussDrv const&)       = delete;
+    void operator=(PrussDrv const&) = delete;
+
+    Pru& get_pru(int pru_num);
+
+    // needed to later wait on event from PRU
+    // sysevt e.g. PRU0_ARM_INTERRUPT
+    void open_sysevt(unsigned sysevt);
+    void pru_send_event(unsigned sysevt);
+    void wait_event_and_clear(unsigned sysevt);
+
+private:
+    static Pru* _prus[TOTAL_PRUS_NUM];
+    static tpruss_intc_initdata _prussintc_init_data;
+    static bool _init_done;
+};
+
+/* default interrupts 
+ *
+ * sysevts: PRU0_PRU1_INTERRUPT=17, PRU1_PRU0_INTERRUPT, PRU0_ARM_INTERRUPT, 
+ *          PRU1_ARM_INTERRUPT, ARM_PRU0_INTERRUPT, ARM_PRU1_INTERRUPT
+ * channels:
+ * 0 = dst PRU0 (PRU1_PRU0_INTERRUPT, ARM_PRU0_INTERRUPT)
+ * 1 = dst PRU1 (PRU0_PRU1_INTERRUPT, ARM_PRU1_INTERRUPT)
+ * 2 = PRU0_ARM_INTERRUPT available as PRU_EVTOUT0
+ * 3 = PRU1_ARM_INTERRUPT available as PRU_EVTOUT1
+ *
+ * terminology the final mapped values are called host:
+ * PRU_EVTOUT0, PRU_EVTOUT1, ...
+ */
+#endif
